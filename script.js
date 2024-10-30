e4ad06c42027ecd667e7f0f9e4b8ab584a5eb37b
Nayeem <mahfujurrahamannayeem@gmail.com>
2024-10-30 22:52:39 +0600

    Creating the Design And give the Functionlity to the button

---
+++ script.js
@@ -0,0 +1,10 @@
+const searchBox = document.querySelector('.search-box');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+
+searchBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log("Button Clicked");
+    
+})
