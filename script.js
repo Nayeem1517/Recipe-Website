7aa96ef2a173c1cde23f139c37b032b5582b6750
Nayeem <mahfujurrahamannayeem@gmail.com>
2024-10-30 23:22:06 +0600

    Fetching The Data From The API And Dynamically Load Them

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ const searchBox = document.querySelector('.search-box');
 const searchBtn = document.querySelector('.searchBtn');
 const recipeContainer = document.querySelector('.recipe-container');
 
+//Function to get recipe
+const fetchRecipe = async(query) =>{
+    const data =await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response =await data.json();
+    response.meals.forEach(meal => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML =`
+        <img src ="${meal.strMealThumb}">
+        <h3> ${meal.strMeal} </h3>
+        <p>${meal.strArea} </p>
+        <p>${meal.strCategory} </p>
+        `
+        recipeContainer.appendChild(recipeDiv)
+    })
+    // console.log(response.meals[0])
+}
+
 
 searchBtn.addEventListener('click',(e)=>{
     e.preventDefault();
-    console.log("Button Clicked");
+    const searchInput = searchBox.value;
+    fetchRecipe(searchInput);
+    // console.log("Button Clicked");
     
 })
